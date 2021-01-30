53d62c372e39b7f8e97b4b114b112ebaea80f8fc
Ran Ding <dingran.nju@gmail.com>
2021-01-30 16:09:28 -0500

    clean up

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,94 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import NextLink from 'next/link';
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  Icon,
+  Link,
+  Stack,
+  Avatar,
+  HStack,
+  Spacer,
+} from '@chakra-ui/react';
 
-export default function Home() {
+import { FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+export default function Home({ children }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Box
+      id='outer-header'
+      bg='#0a0b0c'
+      color='white'
+      zIndex='1000'
+      position='fixed'
+      p={5}
+      w='100%'
+    >
+      <Box id='inner' padding='0' margin='0 auto' width='100%' maxW='1040px'>
+        <Box id='left'>
+          <HStack spacing={8}>
+            <Box id='logo' fontWeight='bold'>
+              Ran Ding
+            </Box>
+            <HStack
+              spacing={6}
+              textTransform='uppercase'
+              opacity={0.8}
+              fontSize='sm'
+            >
+              <Box>About</Box>
+              <Box>Using Ghost</Box>
+              <Box>Machine Learning</Box>
+              <Box>Career</Box>
+              <Box>Projects</Box>
+            </HStack>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <Spacer></Spacer>
+            <HStack
+              spacing={6}
+              textTransform='uppercase'
+              opacity={0.8}
+              fontSize='sm'
+            ></HStack>
+            <Link color='white' opacity={0.8}>
+              <FaTwitter />
+            </Link>
+            <Link color='white' opacity={0.8}>
+              <FaLinkedin />
+            </Link>
+            <Button size='xs' variant='outline' opacity={0.8}>
+              Subscribe
+            </Button>
+          </HStack>
+          {/* <Box id='primary-nav'>
+            <ul class='nav'>
+              <li class='nav-about'>
+                <a href='https://www.dingran.me/about/'>About</a>
+              </li>
+              <li class='nav-using-ghost'>
+                <a href='https://www.dingran.me/tag/using-ghost/'>
+                  Using Ghost
+                </a>
+              </li>
+              <li class='nav-machine-learning'>
+                <a href='https://www.dingran.me/tag/machine-learning/'>
+                  Machine Learning
+                </a>
+              </li>
+              <li class='nav-career'>
+                <a href='https://www.dingran.me/tag/career/'>Career</a>
+              </li>
+              <li class='nav-projects'>
+                <a href='https://www.dingran.me/tag/projects/'>Projects</a>
+              </li>
+            </ul>
+          </Box> */}
+        </Box>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        <Box id='right'></Box>
+      </Box>
+    </Box>
+  );
 }
