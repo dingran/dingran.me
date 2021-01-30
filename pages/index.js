e13d2cd4db71374abf496a4aed8adbac2c954eaa
Ran Ding <dingran.nju@gmail.com>
2021-01-30 18:48:43 -0500

    before refactor

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ const FancyLink = ({ url, children }) => {
 };
 
 export default function Home({ children }) {
-  const [isLargerThan900] = useMediaQuery('(min-width: 900px)');
+  const [largeScreen] = useMediaQuery('(min-width: 800px)');
 
   return (
     <Box
@@ -40,58 +40,72 @@ export default function Home({ children }) {
       px={10}
       w='100%'
     >
-      <Box
+      <Flex
         id='inner'
-        padding='0'
-        margin='0 auto'
-        width='100%'
+        p='0'
+        m='auto'
         maxW='1040px'
+        width='100%'
         overflow='hidden'
         whiteSpace='nowrap'
       >
-        <Box id='left'>
-          <HStack spacing={8}>
-            <Box id='logo' fontWeight='bold'>
-              Ran Ding
-            </Box>
-            <HStack
-              spacing={6}
-              textTransform='uppercase'
-              fontSize='13px'
-              fontWeight='semibold'
-              isTruncated
-            >
-              <FancyLink url='/about'>About</FancyLink>
-              <FancyLink url='/about'>Using Ghost</FancyLink>
-              <FancyLink url='/about'>Machine Learning</FancyLink>
-              <FancyLink url='/about'>Career</FancyLink>
-              <FancyLink url='/about'>Project</FancyLink>
-            </HStack>
-            <Spacer />
-            <HStack spacing={6}></HStack>
-
-            <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
-              <FaTwitter />
-            </Link>
-
-            <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
-              <FaLinkedin />
-            </Link>
-            {isLargerThan900 ? (
-              <Button
-                size='xs'
-                variant='outline'
-                opacity={0.8}
-                _hover={{ opacity: 1.0 }}
-              >
-                Subscribe
-              </Button>
-            ) : (
-              <></>
-            )}
+        <Box
+          flexGrow={1}
+          flexShrink={0}
+          flexBasis='auto'
+          id='left'
+          spacing={8}
+          _after={{
+            content: "''",
+            top: 0,
+            right: 0,
+            zIndex: 1000,
+            width: '40px',
+            height: '140px',
+            background: 'white',
+            // background:
+            // 'linear-gradient( to right, color-mod(var(--darkgrey) l(-5%) a(0)) 0%, color-mod(var(--darkgrey) l(-5%)) 100%)',
+          }}
+        >
+          <Box id='logo' fontWeight='bold'>
+            Ran Ding
+          </Box>
+          <HStack
+            spacing={6}
+            textTransform='uppercase'
+            fontSize='13px'
+            fontWeight='semibold'
+          >
+            <FancyLink url='/about'>About</FancyLink>
+            <FancyLink url='/about'>Using Ghost</FancyLink>
+            <FancyLink url='/about'>Machine Learning</FancyLink>
+            <FancyLink url='/about'>Career</FancyLink>
+            <FancyLink url='/about'>Project</FancyLink>
           </HStack>
         </Box>
-      </Box>
+        {/* <Spacer /> */}
+        <HStack spacing={6} pl={5} flexGrow={0} flexShrink={1} flexBasis='auto'>
+          <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
+            <FaTwitter />
+          </Link>
+
+          <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
+            <FaLinkedin />
+          </Link>
+          {largeScreen ? (
+            <Button
+              size='xs'
+              variant='outline'
+              opacity={0.8}
+              _hover={{ opacity: 1.0 }}
+            >
+              Subscribe
+            </Button>
+          ) : (
+            <></>
+          )}
+        </HStack>
+      </Flex>
     </Box>
   );
 }
