609a5df92e96cf3dbb24722c3027f3b78d14275d
Ran Ding <dingran.nju@gmail.com>
2021-01-30 20:04:09 -0500

    fading out kind of working

--- pages/index.js
+++ pages/index.js
@@ -49,24 +49,7 @@ export default function Home({ children }) {
         overflow='hidden'
         whiteSpace='nowrap'
       >
-        <Box
-          flexGrow={1}
-          flexShrink={0}
-          flexBasis='auto'
-          id='left'
-          spacing={8}
-          _after={{
-            content: "''",
-            top: 0,
-            right: 0,
-            zIndex: 1000,
-            width: '40px',
-            height: '140px',
-            background: 'white',
-            // background:
-            // 'linear-gradient( to right, color-mod(var(--darkgrey) l(-5%) a(0)) 0%, color-mod(var(--darkgrey) l(-5%)) 100%)',
-          }}
-        >
+        <HStack id='left' spacing={8} isTruncated>
           <Box id='logo' fontWeight='bold'>
             Ran Ding
           </Box>
@@ -82,28 +65,40 @@ export default function Home({ children }) {
             <FancyLink url='/about'>Career</FancyLink>
             <FancyLink url='/about'>Project</FancyLink>
           </HStack>
-        </Box>
-        {/* <Spacer /> */}
-        <HStack spacing={6} pl={5} flexGrow={0} flexShrink={1} flexBasis='auto'>
+        </HStack>
+        <Spacer id='spacer' />
+        <HStack
+          spacing={6}
+          flexGrow={0}
+          flexShrink={1}
+          flexBasis='auto'
+          _before={{
+            content: "''",
+            position: 'relative',
+            top: 0,
+            left: '-10px',
+            zIndex: 1000,
+            width: '40px',
+            height: '100%',
+            background: '#0a0b0c',
+            opacity: 0.6,
+            // bgGradient: 'linear( to-l, #0a0b0c,  #ffffff)',
+          }}
+        >
           <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
             <FaTwitter />
           </Link>
-
           <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
             <FaLinkedin />
           </Link>
-          {largeScreen ? (
-            <Button
-              size='xs'
-              variant='outline'
-              opacity={0.8}
-              _hover={{ opacity: 1.0 }}
-            >
-              Subscribe
-            </Button>
-          ) : (
-            <></>
-          )}
+          <Button
+            size='xs'
+            variant='outline'
+            opacity={0.8}
+            _hover={{ opacity: 1.0 }}
+          >
+            Subscribe
+          </Button>
         </HStack>
       </Flex>
     </Box>
