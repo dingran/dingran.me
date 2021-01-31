f0c5dc4c18655f90e3c7b5fe39ec7bc9c1b3c03e
Ran Ding <dingran.nju@gmail.com>
2021-01-31 07:43:11 -0500

    simplified and commented

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,7 @@ import { FaLinkedin, FaTwitter } from 'react-icons/fa';
 const FancyLink = ({ url, children }) => {
   return (
     <NextLink href={url} passHref>
-      <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
-        {children}
-      </Link>
+      <Link>{children}</Link>
     </NextLink>
   );
 };
@@ -32,78 +30,72 @@ export default function Home({ children }) {
 
   return (
     <Box
-      id='outer-header'
-      bg='#0a0b0c'
-      color='white'
-      zIndex='1000'
-      position='fixed'
-      w='100%'
+      bg='rgba(10,11,12)' // based on Casper, #0a0b0c
+      color='white' // text color
+      zIndex='1000' // stay in front
+      position='fixed' // fixed at top
+      w='100%' // fill the whole screen
     >
       <Flex
-        id='inner'
-        maxW='1040px'
-        width='100%'
-        overflow='hidden'
-        m='auto'
-        height='64px'
-        px={10}
-        whiteSpace='nowrap'
-        alignItems='flex-start'
+        id='inner' // is using id the righ way to note to myself what's what?
+        maxW='1040px' // Chakra's shorthabd for max-width
+        m='auto' // centering
+        px={10} // leave some room left and right
+        whiteSpace='nowrap' // ensure signle line
+        height='64px' // specify height, in combination with overflow='hidden' for hiding scroll bar...
+        overflowY='hidden' // hide the ugly x scroll bar, see below
+        alignItems='flex-start' //alignt at the start on the cross axis, i.e. align at top
       >
         <HStack
           id='left'
           spacing={8}
-          isTruncated
-          overflowX='auto'
-          overflowY='hidden'
-          pb='80px'
-          pt='20px'
+          overflowX='auto' // scrollable when screen is narrow
+          pb='80px' // so the the x scrollbar is outside of the 64px height
+          pt='20px' // padding from top to center this
         >
           <Box id='logo' fontWeight='bold'>
             Ran Ding
           </Box>
           <HStack
+            id='nav-links'
             spacing={6}
             textTransform='uppercase'
             fontSize='13px'
             fontWeight='semibold'
-            className={styles.nav}
+            className={styles.navLeft} // adding the :before:hover directly here isn't straigtforward, thus using css module
           >
             <FancyLink url='/about'>About</FancyLink>
             <FancyLink url='/about'>Using Ghost</FancyLink>
             <FancyLink url='/about'>Machine Learning</FancyLink>
             <FancyLink url='/about'>Career</FancyLink>
-            <FancyLink url='/about'>Project</FancyLink>
+            <FancyLink url='/about'>Projects</FancyLink>
           </HStack>
         </HStack>
-        <Spacer id='spacer' />
+        <Spacer /> {/* fill in the horizontal space between the two HStacks*/}
         <HStack
           spacing={6}
-          flexGrow={0}
-          flexShrink={1}
-          flexBasis='auto'
-          height='64px'
+          height='64px' // specify a height makes children element centerned correctly
           _before={{
             content: "''",
-            position: 'relative',
-            left: '-40px',
-            zIndex: 100,
             width: '40px',
-            height: '100%',
-            bgGradient: 'linear( to-r, rgba(10,11,12, 0),  rgba(10,11,12))',
+            position: 'relative',
+            left: '-40px', // place this to overlap with the previous element
+            height: '100%', // fill in the 64px height
+            zIndex: 1000,
+            bgGradient: 'linear( to-r, rgba(10,11,12,0),  rgba(10,11,12))', // Chakra's syntax for making an gradient background
           }}
+          className={styles.navRight} // adding the :before:hover directly here isn't straigtforward, thus using css module
         >
-          <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
+          <Link>
             <FaTwitter />
           </Link>
-          <Link opacity={0.8} _hover={{ opacity: '1.0' }}>
+          <Link>
             <FaLinkedin />
           </Link>
           <Button
             size='xs'
             variant='outline'
-            opacity={0.8}
-            _hover={{ opacity: 1.0 }}
+            _hover={{ opacity: 1 }} //this overwrites the default hover effect which is ugly for outline style button on dark background
           >
             Subscribe
           </Button>
