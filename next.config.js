9dd9f9d0bb6f17aa3dbc42298feff49ae069536d
Ran Ding <dingran.nju@gmail.com>
2021-02-02 08:12:04 -0500

    bumping mdx-js to @next version (2.0.0) and bumping down react to 16 makes mathjax work!

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const withMDX = require('@next/mdx')({
       require('remark-slug'),
       require('remark-autolink-headings'), //TODO: fix; doesn't actually work, ancher didn't wrap the text
     ],
-    rehypePlugins: [require('rehype-katex')],
-    // rehypePlugins: [require('rehype-mathjax')], // this doesn't work due to https://github.com/gatsbyjs/gatsby/issues/25013 and https://github.com/mdx-js/mdx/issues/1148 TODO: update this when mdx 2.0 comes out
+    // rehypePlugins: [require('rehype-katex')],
+    rehypePlugins: [require('rehype-mathjax')], // this doesn't work due to https://github.com/gatsbyjs/gatsby/issues/25013 and https://github.com/mdx-js/mdx/issues/1148 TODO: update this when mdx 2.0 comes out
   },
 });
 module.exports = withMDX({
